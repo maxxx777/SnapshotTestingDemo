673c1b805310949e4a0de145161877f22204b1cf
Maxim Tsvetkov <777.maxxx@gmail.com>
2018-02-11 20:31:37 +0100

    create custom view (EmptyStateView) to test

---
+++ SnapshotTestingDemo/EmptyStateView/EmptyStateView.swift
@@ -0,0 +1,32 @@
+//
+//  EmptyStateView.swift
+//  SnapshotDemo
+//
+//  Created by MAXIM TSVETKOV on 04/02/2018.
+//  Copyright © 2018 MAXIM TSVETKOV. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class EmptyStateView: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView?
+    @IBOutlet weak var labelText: UILabel?
+    @IBOutlet weak var buttonDoSomething: UIButton?
+    
+    static let brandColor = UIColor.black
+    
+    class func instanceFromNib() -> EmptyStateView {
+        let view = UINib(nibName: "EmptyStateView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! EmptyStateView
+        
+        view.labelText?.textColor = brandColor        
+        
+        view.buttonDoSomething?.layer.cornerRadius = 15
+        view.buttonDoSomething?.layer.borderColor = brandColor.cgColor
+        view.buttonDoSomething?.layer.borderWidth = 2.0
+        
+        return view
+    }
+}
