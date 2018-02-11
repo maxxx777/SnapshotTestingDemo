87c232d7044831a9478d8206cc3213743be3cbb1
Maxim Tsvetkov <777.maxxx@gmail.com>
2018-02-11 19:56:32 +0100

    Initial Commit

---
+++ SnapshotTestingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapshotTestingDemo
+//
+//  Created by MAXIM TSVETKOV on 11/02/2018.
+//  Copyright © 2018 MAXIM TSVETKOV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
